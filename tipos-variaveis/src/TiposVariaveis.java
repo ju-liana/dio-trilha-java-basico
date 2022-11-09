601a54133c193f78b672ab45d46b97e93fefcb7c
Juliana <juliana@yahoo.com.br>
2022-11-09 20:52:53 -0300

    concatenacao

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Juliana Daniela";
+
+
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 314;
+        System.out.print(VALOR_DE_PI);
+
+        
     }
 }
